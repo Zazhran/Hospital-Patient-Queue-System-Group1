cbd660dd6d786039ee1fcd07a42077fca9364c24
Zahran <zazhran03@gmail.com>
2026-04-17 20:22:53 +0700

    File Preparation

--- Student4 (Zahran Hilal)/DoctorScheduleMap.java
+++ Student4 (Zahran Hilal)/DoctorScheduleMap.java
@@ -118,25 +118,4 @@ public class DoctorScheduleMap {
     }
 
 
-    public static void main(String[] args) {
-        addAppointment("Andi", "P001", "Pemeriksaan rutin");
-        addAppointment("Andi", "P002", "Kontrol lanjutan");
-        addAppointment("Budi", "P003", "Konsultasi awal");
-
-        showSize();
-
-        listDoctors();
-
-        getAppointments("Andi");
-
-        removeDoctor("Budi");
-
-        getAppointments("Budi");
-
-        getAppointments("Pigeon");
-
-        showSize();
-    }
-
-
 }
