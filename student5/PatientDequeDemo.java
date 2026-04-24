22aa6055653931c35d3dc3dd236c5ab5914ddd56
Andre <andrefourme2@gmail.com>
2026-04-24 11:08:28 +0700

    Implement PatientDequeDemo for patient management

--- student5/PatientDequeDemo.java
+++ student5/PatientDequeDemo.java
@@ -1,1 +1,26 @@
+public class PatientDequeDemo {
+  public static void main(String[] atgs) {
 
+    PatientDeque deque = new PatientDeque();
+
+    // Tambah pasien
+    deque.addRear(new Patient("p001", "Budiono Siregar", 35, "demam"));
+    deque.addRear(new Patient("p002:, "Siti Azzizah", 28, "sakit kepala"));
+    deque.addFront(new Patient("p003", "Ahmad Fauzi", 52, "nyeri dada")); // prioritas
+
+    deque.display();
+
+  System.out.println("Depan: " + deque.peekFront().getName());
+  System.out.println("Belakang: + deque.peekRear().getName());
+
+  deque.removeFront();
+  deque.removeRear();
+
+  deque.display();
+
+  deque.removeFront();
+  deque.removeFront(); // harus muncul eror
+  }
+}
+    
+    deque
