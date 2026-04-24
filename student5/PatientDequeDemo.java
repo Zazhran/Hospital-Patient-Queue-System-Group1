ca8dcd624434bb8561beca715a9979e7f3b5dad6
Andre <andrefourme2@gmail.com>
2026-04-24 11:29:38 +0700

    Fix main method and update patient details

--- student5/PatientDequeDemo.java
+++ student5/PatientDequeDemo.java
@@ -1,26 +1,24 @@
 public class PatientDequeDemo {
-  public static void main(String[] atgs) {
+    public static void main(String[] args) {
 
-    PatientDeque deque = new PatientDeque();
+        PatientDeque deque = new PatientDeque();
 
-    // Tambah pasien
-    deque.addRear(new Patient("p001", "Budiono Siregar", 35, "demam"));
-    deque.addRear(new Patient("p002:, "Siti Azzizah", 28, "sakit kepala"));
-    deque.addFront(new Patient("p003", "Ahmad Fauzi", 52, "nyeri dada")); // prioritas
+        // Tambah pasien
+        deque.addRear(new Patient("P001", "Budi Santoso", 35, "demam"));
+        deque.addRear(new Patient("P002", "Siti Nuraini", 28, "sakit kepala"));
+        deque.addFront(new Patient("P003", "Ahmad Fauzi", 52, "nyeri dada")); // prioritas
 
-    deque.display();
+        deque.display();
 
-  System.out.println("Depan: " + deque.peekFront().getName());
-  System.out.println("Belakang: + deque.peekRear().getName());
+        System.out.println("Depan: " + deque.peekFront().getName());
+        System.out.println("Belakang: " + deque.peekRear().getName());
 
-  deque.removeFront();
-  deque.removeRear();
+        deque.removeFront();
+        deque.removeRear();
 
-  deque.display();
+        deque.display();
 
-  deque.removeFront();
-  deque.removeFront(); // harus muncul eror
-  }
+        deque.removeFront();
+        deque.removeFront(); // harus muncul error
+    }
 }
-    
-    deque
