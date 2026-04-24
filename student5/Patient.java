690fbe7f26125bdf4caab25d65b242b9abdfb92e
Andre <andrefourme2@gmail.com>
2026-04-24 11:28:15 +0700

    Update Patient.java

--- student5/Patient.java
+++ student5/Patient.java
@@ -1,31 +1,29 @@
 public class Patient {
-  private String id;
-  private String name;
-  private int age;
-  private String ailment;
+    private String id;
+    private String name;
+    private int age;
+    private String ailment;
 
-  public Patient(String id, String name, int age, String ailment) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.ailment = ailment;
- }
+    public Patient(String id, String name, int age, String ailment) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.ailment = ailment;
+    }
 
-public String getId() {
-  return id;
- }
+    public String getId() {
+        return id;
+    }
 
-public String getName() {
-  return name;
- }
+    public String getName() {
+        return name;
+    }
 
-public String getAge() {
-  return age;
- }
+    public int getAge() {
+        return age;
+    }
 
-public String getAilment() {
-  return ailment;
- }
+    public String getAilment() {
+        return ailment;
+    }
 }
-  
-  
